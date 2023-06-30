cd25be973b2f1a52a40e37cbc3aeb4eb1ec4b3d7
Renu Desai <renud.91@gmail.com>
2023-06-30 13:28:38 +0100

    consumed API details and added global variables

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,6 @@
 // eslint-disable-next-line no-unused-vars
-import { request } from './helpers';
+import { request } from "./helpers";
+import axios from "axios";
 
 /**
  * Pull vehicles information
@@ -8,5 +9,36 @@ import { request } from './helpers';
  */
 // TODO: All API related logic should be made inside this function.
 export default async function getData() {
-  return [];
+    try {
+        const response = await axios.get("/api/vehicles.json");
+        const vehicleList = response.data;
+        const detailedVehiclePromises = vehicleList.map(async (vehicle) => {
+            try {
+                const detailResponse = await axios.get(vehicle.apiUrl);
+                const detailedVehicle = detailResponse.data;
+                if (detailedVehicle.price) {
+                    return {
+                        ...vehicle,
+                        ...detailedVehicle,
+                    };
+                }
+            } catch (error) {
+                if (error.response && error.response.status === 404) {
+                    console.log(
+                        `API endpoint not found for vehicle ${vehicle.id}`
+                    );
+                } else {
+                    console.error(
+                        `Error fetching vehicle details for ${vehicle.id}:`,
+                        error
+                    );
+                }
+            }
+            return null;
+        });
+        const detailedVehicles = await Promise.all(detailedVehiclePromises);
+        return detailedVehicles.filter((vehicle) => vehicle !== null);
+    } catch (error) {
+        console.error("Error fetching vehicle list:", error);
+    }
 }
